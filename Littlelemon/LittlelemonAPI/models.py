887540f11ae91406dc04dfae1be4791b601347b4
Ngozi Uchenna <boluwatifen3@gmail.com>
2024-08-01 20:21:39 +0100

    updates

--- Littlelemon/LittlelemonAPI/models.py
+++ Littlelemon/LittlelemonAPI/models.py
@@ -9,4 +9,24 @@ class Category(models.Model):
         return self.name
 
 class MenuItem(models.Model):
-    name
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_delivered = models.BooleanField(default=False)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.quantity} of {self.menu_item.name}'
--- Littlelemon/LittlelemonAPI/views.py
+++ Littlelemon/LittlelemonAPI/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import Category, MenuItem, Order, OrderItem
+from .serializers import CategorySerializer, MenuItemSerializer, OrderSerializer, OrderItemSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class MenuItemViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
